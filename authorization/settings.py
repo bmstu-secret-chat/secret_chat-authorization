3ced54a0513bde7d17e23c17ab17ca05992c967c
arklem <jrsy.tdh@yandex.ru>
2024-12-11 11:03:51 +0300

    update lifetime of refresh token

--- authorization/settings.py
+++ authorization/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 # Настройка кэширования с использованием Redis
